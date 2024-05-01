c2022c693f7afb2f42f23368d056970ebedfd4e6
travisburns <travisjb0@gmail.com>
2024-04-30 19:24:05 -0700

    Finished question 3 and 4 for lab 3

--- Recursion_Core5/Palindromes/Program.cs
+++ Recursion_Core5/Palindromes/Program.cs
@@ -1,4 +1,28 @@
-﻿using System;
+﻿//Using the Visual Studio Solution provided in the starting files as a starting point,
+//write a recursive method to determine if a string is a palindrome.  The method should take a
+//string s as its parameter and return a bool. Use your method to write an application that asks
+//the user to enter a word or phrase from the keyboard and then tells the user whether or not the
+//text that they entered is a palindrome.  Capitalization whitespace and punctuation should be
+//removed before calling your function.
+
+
+//Input 
+    //user enters a word or phrase, string 
+
+//Proccessing
+    //takes string s as its parameter 
+    //Capitalization and punctuation should be removed from the string s from input
+    //calcualate the string to a reverse and compare the first instance to the second, this equals a palindrome. 
+    //checks whether the word is a palindrome or not 
+        //if word is palindrome then return a bool - (Whether the string is the same backwards and forwards)
+
+
+//Output
+    //outputs whether the string entered is a palindrome or not. 
+
+
+
+using System;
 
 namespace Palindromes
 {
@@ -6,7 +30,43 @@ namespace Palindromes
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Enter a word or phrase:");
+            string input = Console.ReadLine();
+
+            // Check if the input string is a palindrome
+            bool isPalindrome = Palindrome(input);
+
+            // Output the result
+            if (isPalindrome)
+                Console.WriteLine("The entered text is a palindrome.");
+            else
+                Console.WriteLine("The entered text is not a palindrome.");
+
+
+
+
+            static bool Palindrome(string str)
+            {
+                // Remove capitalization, whitespace, and punctuation
+                string polishedInput = str.ToLower();
+                polishedInput = new string(Array.FindAll(polishedInput.ToCharArray(), char.IsLetterOrDigit));
+
+                // Base case: if the string is empty or has only one character, it's a palindrome
+                if (polishedInput.Length <= 1)
+                    return true;
+
+                // Compare the first and last characters
+                if (polishedInput[0] != polishedInput[polishedInput.Length - 1])
+                    return false;
+
+                // check the substring without the first and last characters by calling the recursion
+                return Palindrome(polishedInput.Substring(1, polishedInput.Length - 2));
+            }
+
+
+
+
         }
     }
 }
--- Recursion_Core5/TowerOfHanoi/Program.cs
+++ Recursion_Core5/TowerOfHanoi/Program.cs
@@ -1,4 +1,27 @@
-﻿using System;
+﻿//write a recursive method to solve the Tower of Hanoi puzzle with n discs.
+//The method should take an integer n representing the number of discs and a
+//string representing the name of the tower on which the discs start,
+//a string representing the name of the tower to which the discs must be
+//moved and a string representing the name of the extra tower as its parameters.
+//The method does not return a value but displays a message describing each move.
+//Use your method to write an application that asks the user to enter how many discs
+//they would like and then displays the moves necessary to solve the puzzle.
+
+
+//Input 
+//integer n that represents the number of disks
+
+//Processing
+//Check if n equals 1, the base case. 
+ //if n does equal 1, then move the disk from the starting tower to the ending tower
+   //else call the recursion at (n -1), moving the discs to the goal tower by use of
+   //the holding tower at first.
+   //then you want to continue the iteration, moving the n'th disk from the start tower to goal tower
+   //then finally move the disks from the holding tower to the goal tower, you need to use the starting tower as a holding tower as well
+//Output
+//console message that will describe each move. 
+
+using System;
 
 namespace TowerOfHanoi
 {
@@ -6,7 +29,24 @@ namespace TowerOfHanoi
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter the number of discs:");
+            int discNumber = Convert.ToInt32(Console.ReadLine());
+
+            // Solve the Tower of Hanoi puzzle
+            TowerOfHanoi(discNumber, "Start", "Goal", "Holding");
+        }
+
+        static void TowerOfHanoi(int n, string start, string goal, string holding)
+        {
+            if (n == 1)
+            {
+                Console.WriteLine($"Move disk 1 from {start} to {goal}");
+                return;
+            }
+
+            TowerOfHanoi(n - 1, start, holding, goal);
+            Console.WriteLine($"Move disk {n} from {start} to {goal}");
+            TowerOfHanoi(n - 1, holding, goal, start);
         }
     }
 }
